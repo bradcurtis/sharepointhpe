8aa4984787c8595c53c0441ea021d3e407f63619
johnsonpace <bradcurtis74@msn.com>
2016-04-11 07:38:02 -0700

    updated

--- TopNav/MyCustomSiteMapProvider.cs
+++ TopNav/MyCustomSiteMapProvider.cs
@@ -10,10 +10,12 @@ namespace TopNav
     {
         public override SiteMapNodeCollection GetChildNodes(SiteMapNode node)
         {
-            ULSLog2013.LogMessage("I was called");
-            PortalSiteMapNode pNode = node as PortalSiteMapNode;
+            ULSLog2013.LogMessage("Child Nodes was called");
+           /* PortalSiteMapNode pNode = node as PortalSiteMapNode;
             if (pNode != null)
             {
+                ULSLog2013.LogMessage("We currently have portal site node map");
+
                 if (pNode.Type == NodeTypes.Area && pNode.WebId == SPContext.Current.Site.RootWeb.ID)
                 {
                     SiteMapNodeCollection nodeColl = base.GetChildNodes(pNode);
@@ -35,11 +37,49 @@ namespace TopNav
                 }
                 return base.GetChildNodes(pNode);
             }
+
+            ULSLog2013.LogMessage("We DO NOT have a node map");
             SiteMapNode childNodeNew = new SiteMapNode(this,
                                                             "github", "http://www.github.com",
                                                             "git hub link");
 
-            return new SiteMapNodeCollection(childNodeNew);
+            SiteMapNode childNodeNew1 = new SiteMapNode(this,
+                                                             "msn",
+                                                             "http://www.msn.com", "MSN link");
+
+           // childNodeNew.ChildNodes.Add(childNodeNew1);
+            SiteMapNodeCollection test1 = new SiteMapNodeCollection();
+           // childNodeNew.ChildNodes.Add(childNodeNew1);
+           // test1.Add(childNodeNew);
+           //test1.Add(childNodeNew1);
+           // childNodeNew.ChildNodes.Add(childNodeNew1);
+            return base.GetChildNodes(childNodeNew);*/
+
+
+         
+                   
+                    SiteMapNode childNode = new SiteMapNode(this,
+                    "Node1", "http://www.microsoft.com", "Microsoft");
+
+                    SiteMapNode childNode1 = new SiteMapNode(this,
+                    "Node11", "http://support.microsoft.com", "Support");
+
+                  
+
+                    SiteMapNodeCollection test = new SiteMapNodeCollection();
+                    SiteMapNodeCollection test2 = new SiteMapNodeCollection();
+                    SiteMapNodeCollection test3 = new SiteMapNodeCollection();
+                    test.Add(childNode);
+                    test2.Add(childNode1);
+                    childNode.ChildNodes = test2;
+                    childNode1.ChildNodes = test3;
+                   // base.AddNode(childNode);
+                   // base.AddNode(childNode1);
+                   // return base.GetChildNodes(childNode);
+                    return test;
+
+                   
+                
         }
 
         
@@ -68,7 +108,7 @@ namespace TopNav
 
         public override void Initialize(string name, System.Collections.Specialized.NameValueCollection attributes)
         {
-            ULSLog2013.LogMessage("I was Called");
+            ULSLog2013.LogMessage("Initialize was called");
             ULSLog2013.LogMessage(name);
            
             foreach (string s in attributes.Keys)
